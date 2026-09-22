d53a04a57c8e145309c93aff2bfb872fe9b1cb74
Mihail Kalantaev <mihailkalantaev@gmail.com>
2026-09-22 14:21:19 +0700

    lab2: fix correct time

--- 25215/m.kalantaev/lab2/ex_time.c
+++ 25215/m.kalantaev/lab2/ex_time.c
@@ -19,7 +19,7 @@ int main(void)
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
            sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year, sp->tm_hour,
+           sp->tm_year + 1900, sp->tm_hour,
            sp->tm_min, tzname[sp->tm_isdst]);
 
     return 0;
